52310af735b6d6f32b1e4972b4d70f2704c08ff6
Shreya Parashar <shreyaparashar101@yahoo.com>
2026-04-20 23:46:04 +0530

    feat : add substitution, reward function, cost estimator and data files - member_3

---
+++ data/indian_ingredients.py
@@ -0,0 +1,48 @@
+INDIAN_INGREDIENTS = [
+    # Spices
+    "cumin", "coriander", "turmeric", "ginger", "garlic", "mustard seeds",
+    "fenugreek", "cardamom", "clove", "cinnamon", "black pepper", "red chili",
+    "green chili", "asafoetida", "saffron", "fennel seeds", "star anise",
+    "nutmeg", "mace", "poppy seeds", "bay leaf", "curry leaves", "carom seeds",
+    "dry mango powder", "chaat masala", "garam masala", "kasuri methi",
+    "black cardamom", "stone flower", "dried red chili",
+
+    # Vegetables
+    "onion", "tomato", "potato", "spinach", "okra", "eggplant", "brinjal",
+    "bitter gourd", "bottle gourd", "ridge gourd", "snake gourd", "drumstick",
+    "raw banana", "raw papaya", "yam", "taro root", "lotus stem",
+    "cauliflower", "cabbage", "peas", "carrot", "radish", "beetroot",
+    "french beans", "cluster beans", "colocasia", "raw jackfruit",
+
+    # Lentils and Legumes
+    "toor dal", "moong dal", "chana dal", "urad dal", "masoor dal",
+    "rajma", "chickpea", "black eyed peas", "horse gram", "green moong",
+    "lobia", "soybean",
+
+    # Grains and Flours
+    "rice", "wheat", "atta", "maida", "besan", "sooji", "poha",
+    "ragi", "bajra", "jowar", "barley", "sabudana", "bread",
+
+    # Dairy
+    "milk", "curd", "yogurt", "paneer", "ghee", "butter", "cream",
+    "khoya", "condensed milk", "buttermilk",
+
+    # Oils and Fats
+    "coconut oil", "mustard oil", "groundnut oil", "sesame oil",
+    "sunflower oil", "refined oil",
+
+    # Nuts and Seeds
+    "cashew", "almond", "peanut", "sesame seeds", "coconut",
+    "watermelon seeds", "pumpkin seeds", "flax seeds", "walnut", "pistachio",
+
+    # Fruits
+    "mango", "banana", "coconut", "tamarind", "lemon", "lime",
+    "amla", "raw mango", "dates", "raisins", "pomegranate",
+
+    # Sweeteners
+    "sugar", "jaggery", "honey", "mishri",
+
+    # Others
+    "salt", "vinegar", "rose water", "kewra water", "baking soda",
+    "baking powder", "corn flour"
+]
